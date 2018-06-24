db34e106a005957a8ebddcc676373522cd68bd74
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-24 10:06:51 -0500

    Pagina del artista, nueva ruta, parametro url y servicio

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-artista',
   templateUrl: './artista.component.html',
   styles: []
 })
-export class ArtistaComponent implements OnInit {
+export class ArtistaComponent {
 
-  constructor() { }
+  constructor( private router: ActivatedRoute ) {
 
-  ngOnInit() {
+    this.router.params.subscribe ( params => {
+      console.log(params['id']);
+    });
   }
 
 }
