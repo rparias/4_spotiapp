6b3ec4ccbde4508d19da6145b5dfbd64d254b1a1
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-24 00:36:21 -0500

    Verificacion de imagen con pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { HttpClientModule } from '@angular/common/http';
 // Importar rutas
 import { ROUTES } from './app.routes';
 
+// Importar pipe
+import { NoimagePipe } from './pipes/noimage.pipe';
+
 
 
 
@@ -23,7 +26,8 @@ import { ROUTES } from './app.routes';
     HomeComponent,
     SearchComponent,
     ArtistaComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe
   ],
   imports: [
     BrowserModule,
